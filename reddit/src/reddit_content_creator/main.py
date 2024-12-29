bdfcdaa24bc3ea3cb39d2cc63d3467be477ac2a6
Mikatux <6116145+Mikatux@users.noreply.github.com>
2024-12-29 11:31:57 -0500

    Add config for container image

--- reddit/src/reddit_content_creator/main.py
+++ reddit/src/reddit_content_creator/main.py
@@ -6,11 +6,6 @@ from reddit_content_creator.crew import RedditContentCreator
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
 def run():
     """
     Run the crew.
@@ -20,7 +15,6 @@ def run():
         'post_subject': 'How to use crewai to analyse and generate custom reddit post'
     }
     RedditContentCreator().crew().kickoff(inputs=inputs)
-
 
 def train():
     """
